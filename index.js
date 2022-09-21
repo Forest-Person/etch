28ce32e3842978be9b4bdb37e5936bc08ff53f2a
matthew <permahuman@gmail.com>
2022-09-21 07:45:01 -0700

    Add foreach method that add event listener to row nodelist and then gets the array index of the row nodelist which is being clicked on.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/*
+First a grid and grid class are created efore the loop is created , 
+then a loop starts that creates the div for the column and a a class,
+then a loop runs fiuve times appending the row which is created per loop to the column
+finally the whole column and five rows are appended to the grid.
+after the loop the grid is appened to the body of the dom.
+
+Next we need to use query selector all and add event handlers which listen
+for a click or touch event.
+*/
+
+
 let columns = 5
 let rows = 5
 
@@ -8,7 +20,7 @@ for (let i = 0; i <= columns; i++) {
 
   const column = document.createElement('div');
   column.className = 'column' 
-
+                                                     
   for (let j = 0; j <= rows; j ++) {
 
     const row = document.createElement('div');
@@ -23,15 +35,24 @@ for (let i = 0; i <= columns; i++) {
 document.body.append(grid)
 
 
-document.querySelectorAll('.row').forEach(
-  item => {
-  
-  item.addEventListener('click',
-  
-  () => {
-  
-  item.style.backgroundColor = 'blue'
 
+
+var row = document.querySelectorAll('.row');
+
+row.forEach(function(item){
+  item.addEventListener('mouseover', function(event){
+
+   row[Array.from(row).indexOf(event.target)].classList.toggle('toggleBlue')
+  })
 })
-})
+
+function checkIndex(event){
+  console.log(Array.from(row).indexOf(event.target))
+  
+  return Array.from(row).indexOf(event.target)
+  
+}
+
+  
+
 
