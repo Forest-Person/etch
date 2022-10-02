85b5460b2ac3394c44064401fa70aea750757eec
matthew <permahuman@gmail.com>
2022-10-01 19:42:54 -0700

    Remove toggle classes from css. Create toggleblack event listener function. Create erase grid function. Create eventlisteners for row class and erase grid button.

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-/*
-First a grid and grid class are created efore the loop is created , 
-then a loop starts that creates the div for the column and a a class,
-then a loop runs fiuve times appending the row which is created per loop to the column
-finally the whole column and five rows are appended to the grid.
-after the loop the grid is appened to the body of the dom.
+let row = document.querySelectorAll('.row');  //create golbal row query all for test on erase grid funciton
 
-Next we need to use query selector all and add event handlers which listen
-for a click or touch event.
-*/
-
-
-
-function CreateGrid() {
+function CreateGrid() {    //Function which creates a grid and appends it to the gridcontainer in the body
 
   gridCont = document.querySelectorAll('.gridContainer')
 
   gridCont.forEach((item) => item.removeChild(item.firstChild))
 
-grid = document.createElement('div')
-grid.className = 'grid'
+  grid = document.createElement('div')
+  grid.className = 'grid'
   
  
 let columns = prompt('What size of grid do you want?')
 let rows = columns
-
 
 
 for (let i = 0; i <= columns; i++) {
@@ -38,47 +26,56 @@ for (let i = 0; i <= columns; i++) {
     column.append(row)
   }
   grid.append(column)
-
-  
-  
   
 }
-
 
 const gridContainer = document.querySelector('.gridContainer')
 
-gridContainer.append(grid)
+gridContainer.append(grid)  //Grid ^^^ has been created and appened to the DOM. 
 
-let row = document.querySelectorAll('.row');
-
-  row.forEach(
-    (item)=>item.addEventListener('mouseover',
-    
-    (event) => row[Array.from(row).indexOf(event.target)].classList = 'toggleBlue')
-  )
+ToggleGridBlack()
 
 }
 
-newGrid = document.querySelector('.newGrid')
-
-newGrid.addEventListener('click', CreateGrid)
 
 
+newGrid = document.querySelector('.newGrid')  
+
+newGrid.addEventListener('click', CreateGrid)  //Creates an event listener for the make new grid button that creates a new grid.
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+eraseGridButton = document.querySelector('.eraseGrid') 
 
   
+  //trying to create funciton to remove toggle black event listener and add white toggle class
 
+eraseGridButton.addEventListener('click' , EraseGrid)
+
+
+function EraseGrid(){
+  let row = document.querySelectorAll('.row');
+  row.forEach((item) =>
+  
+  
+  item.style.background = 'white'
+  )
+}
+
+ 
+function ToggleGridBlack() {
+
+  let row = document.querySelectorAll('.row');
+
+  row.forEach((item) =>
+    
+    item.addEventListener('mouseover', () =>
+  item.style.background = 'black'
+  )
+  )
+     //For each loop with event listener that toggles 
+                                                 //the nodelist of .row class to a different class.
+    
+  
+  
+}
 
